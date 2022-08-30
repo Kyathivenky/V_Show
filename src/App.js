f44faae2d6f932d1d6dc920940f7010189f3c16d
Kyathi Veera Venkat Mallipudi <97524741+Kyathivenky@users.noreply.github.com>
2022-08-30 20:42:00 +0530

    using Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+ import {Switch,Route} from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div >
-      hi
-    </div>
-  );
+  return <Switch>
+    <Route path="/">
+      hi this is name
+    </Route>
+  </Switch>
+  
 }
+
 
 export default App;
